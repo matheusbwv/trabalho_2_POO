a336e331fc87878f1ed12f0f2e686d973cc29fcf
matheusbwv <matheus.wenes@gmail.com>
2022-04-09 18:19:47 -0300

    Atividade_POO

--- src/server.ts
+++ src/server.ts
@@ -91,25 +91,8 @@
 
 
 //}
-class pessoas{
-    idade: number;
-    nome: string;
 
-    constructor(idade:number,nome:string){
-        this.idade = idade
-        this.nome = nome
-    }
-idade_meses(){
-    return this.idade * 12
-    }
-
-} 
-
-let t1 = new pessoas(55, "matheus");
-console.log('a idade:  ',t1.idade);
-console.log('nome: ', t1.nome);
-console.log('idade em meses: ', t1.idade_meses());
-
+// 1ª questão:
 
 function newFunction() {
     const Prompt = require("prompt-sync")();
@@ -132,3 +115,24 @@ function newFunction() {
 
     console.log(soma);
 }
+
+
+// 2ª questão:
+class pessoas{
+    idade: number;
+    nome: string;
+
+    constructor(idade:number,nome:string){
+        this.idade = idade
+        this.nome = nome
+    }
+idade_meses(){
+    return this.idade * 12
+    }
+
+} 
+
+let t1 = new pessoas(55, "matheus");
+console.log('a idade:  ',t1.idade);
+console.log('nome: ', t1.nome);
+console.log('idade em meses: ', t1.idade_meses());
