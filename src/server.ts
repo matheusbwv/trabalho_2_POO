bc5cf31acab3bfa2769bc8dc7aa16a18d7185df2
matheusbwv <matheus.wenes@gmail.com>
2022-04-13 18:42:52 -0300

    Atividade_POO

--- src/server.ts
+++ src/server.ts
@@ -2,42 +2,42 @@
 
 const Prompt = require("prompt-sync")();
 
-//     let num1 = 0;
-//     let num2 = 0;
-//     let soma = 0;
+    let num1 = 0;
+    let num2 = 0;
+    let soma = 0;
 
-//     do {
-//         num1 = parseInt(Prompt('digite um numero 1: '));
-//         num2 = parseInt(Prompt('digite um numero 2: '));
-//     } while (num1 < 0 && num2 < 0);
-
-
+    do {
+        num1 = parseInt(Prompt('digite um numero 1: '));
+        num2 = parseInt(Prompt('digite um numero 2: '));
+    } while (num1 < 0 && num2 < 0);
 
 
-//     for (let i = num1; i < num2; i++) {
-//         soma = soma + i;
-//     }
-
-//     console.log(soma);
-// }
 
 
-// // 2ª questão:
-// class pessoas{
-//     idade: number;
-//     nome: string;
+    for (let i = num1; i < num2; i++) {
+        soma = soma + i;
+    }
 
-//     constructor(idade:number,nome:string){
-//         this.idade = idade
-//         this.nome = nome
-//     }
-// idade_meses(){
-//     return this.idade * 12
-//     }
+    console.log(soma);
+}
 
-// } 
 
-// let t1 = new pessoas(55, "matheus");
-// console.log('a idade:  ',t1.idade);
-// console.log('nome: ', t1.nome);
-// console.log('idade em meses: ', t1.idade_meses());
+// 2ª questão:
+class pessoas{
+    idade: number;
+    nome: string;
+
+    constructor(idade:number,nome:string){
+        this.idade = idade
+        this.nome = nome
+    }
+idade_meses(){
+    return this.idade * 12
+    }
+
+} 
+
+let t1 = new pessoas(55, "matheus");
+console.log('a idade:  ',t1.idade);
+console.log('nome: ', t1.nome);
+console.log('idade em meses: ', t1.idade_meses());
